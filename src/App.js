be5a7bfb7aa47dd1f5b1fa41412ef0801321953e
Jaskaran Singh <thecodersingh@gmail.com>
2022-06-14 23:44:51 -0700

    Create footer, add style

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
 import Publications from "./components/Publications/Publications";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 import { ProjectsData } from "./components/Projects/ProjectsData";
 import { PublicationsData } from "./components/Publications/PublicationsData";
 
 import "./App.scss";
-import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Skills />
       <Publications publications={PublicationsData} />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import "./Footer.scss";
+
+const Footer = () => {
+    return (
+        <div id="footer">Designed &amp; Developed by <a href="/">Jaskaran Singh</a></div>
+    );
+};
+
+export default Footer;
