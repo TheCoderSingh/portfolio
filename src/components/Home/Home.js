c41e63abdb3d68b0a7fa8165f1d576be0f30b5ed
Jaskaran Singh <thecodesingh@gmail.com>
2023-01-02 19:34:58 -0800

    Fix spacing issue on Home and Technologies

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
           </div>
         </div>
         <div className="main-img">
-          <img src={Jas} alt="Jaskaran Picture" />
+          <img src={Jas} alt="Jaskaran" />
         </div>
       </div>
       <div className="contact-btn">
